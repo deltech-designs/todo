bd0b21cad6f7d76baea72b97e68975728adf8cb5
deltechdesigns <deltechdesigns@gmail.com>
2023-04-26 22:36:20 +0100

    complete the navbar

--- script.js
+++ script.js
@@ -1,36 +1,69 @@
+/* ============== Adjusting the size of task title input ============= */
 
-const hiddenNav = document.querySelector('.side-nav'); 
+const taskName = document.querySelector('.task-name'); 
+const adjustHeight = () => {
+    taskName.style.height = 'auto'; 
+    taskName.style.height = taskName.scrollHeight + 'px'; 
+}
+taskName.addEventListener('input', adjustHeight);
 
-// Navbar toggle 
-const showNav = document.querySelector('.show').onclick = () =>{
-    hiddenNav.classList.toggle('hidden');
+const plusModal = document.querySelector('.modal-plus'); 
+const plusIcon = document.querySelector('.plus-icon');
+const plusModalCancel = document.querySelector('.plus-modal-cancel')
+
+const openModal = function(ev){
+    ev.preventDefault();
+    plusModal.classList.remove('hidden'); 
+    plusModal.style.transition = '0.5s'
 }
 
 
-// Accordion checker 
-const acc = document.querySelector('.accordion-history').addEventListener('click', () => {
-    const accDetail = document.querySelector('.accordion-detail').classList.toggle('accordion-detail-show'); 
-    const arrowDown = document.querySelector('.arrow').classList.toggle('fa-arrow-alt-circle-down');
-}); 
+const closeModal = function(ev){
+    ev.preventDefault();
+    plusModal.classList.add('hidden'); 
+}
+
+plusIcon.addEventListener('click', openModal);
+plusModalCancel.addEventListener('click', closeModal);
+
+// const openModal = document.querySelector('.'); 
+// const openModal = function(){
+//     // alert('Hello'); 
+//     plusModal.classList.remove('hidden'); 
+// }
+// plusModal.addEventListener('click', openModal ); 
 
 
-const addPlus = document.querySelector('.add-task-plus').addEventListener('click', () => {
-    alert('Hey'); 
-}); 
 
-// Add task modal box
-const addTask = document.querySelectorAll('.add-task').forEach((item) => {
-    item.addEventListener('click', () => {
-        alert('alert'); 
-    })
-}); 
+// const closeModal = function(){
+//     pl
+// }
 
-const numbers = [33, 2, 8]
-numbers.sort((a,b) => {
-    return a - b; 
-});
 
-console.log(numbers);
-// console.log(addTask);
 
-// console.log(addTask)
+// const hiddenNav = document.querySelector('.side-nav'); 
+// // Navbar toggle 
+// const showNav = document.querySelector('.show').onclick = () =>{
+//     hiddenNav.classList.toggle('hidden');
+// }
+
+
+// // Accordion checker 
+// const acc = document.querySelector('.accordion-history').addEventListener('click', () => {
+//     const accDetail = document.querySelector('.accordion-detail').classList.toggle('accordion-detail-show'); 
+//     const arrowDown = document.querySelector('.arrow').classList.toggle('fa-arrow-alt-circle-down');
+// }); 
+
+
+// const addPlus = document.querySelector('.add-task-plus').addEventListener('click', () => {
+//     alert('Hey'); 
+// }); 
+
+// // Add task modal box
+// const addTask = document.querySelectorAll('.add-task').forEach((item) => {
+//     item.addEventListener('click', () => {
+//         alert('alert'); 
+//     })
+// }); 
+
+
