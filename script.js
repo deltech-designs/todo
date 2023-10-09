028d2b05827639164d1f3af049d09aba3fa1bf55
deltechdesigns <deltechdesigns@gmail.com>
2023-10-09 13:56:20 +0100

    working on it

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const taskName = document.querySelector('.task-title'); 
+const taskTitle = document.querySelector('.task-title'); 
 
 const handBudger = document.querySelector('.handbudger'); 
 const sideNav = document.querySelector('.side-nav'); 
@@ -8,14 +8,22 @@ const openModal = document.querySelectorAll('.btn-task');
 const modalBox = document.querySelector('.modal-box');
 const closeModal = document.querySelector('.close-modal-box'); 
 
+const addTask = document.querySelector(".add-task"); 
+let storage = {
+    todo:[], 
+    doing:[],
+    done:[]
+} 
+
+// console.log(storage)
 
 
-/* ============== Adjusting the size of task title input ============= */
+/* ============== Adjusting the sizex of task title input ============= */
 const adjustHeight = () => {
-    taskName.style.height = 'auto'; 
-    taskName.style.height = taskName.scrollHeight + 'px'; 
+    taskTitle.style.height = 'auto'; 
+    taskTitle.style.height = taskTitle.scrollHeight + 'px'; 
 }
-taskName.addEventListener('input', adjustHeight);
+taskTitle.addEventListener('input', adjustHeight);
 
 
 handBudger.addEventListener('click', () => {
@@ -33,67 +41,116 @@ closeModal.addEventListener('click', () => {
     modalBox.classList.add('hidden'); 
 })
 
+const createListItem = function (text){
+    const listItem = document.createElement("li"); 
+    const label = document.createElement("label")
+    const checkbox = document.createElement("input"); 
+    checkbox.setAttribute("type", "checkbox")
+    checkbox.classList.add('circle-checkbox'); 
+    label.appendChild(checkbox); 
+    label.appendChild(document.createTextNode(text))
+    listItem.appendChild(label)
+}
 
-// Open the modal box 
-// const openModa = function(ev){
+
+const addItemToList = function(text) {
+    const getStoredObject = JSON.parse(localStorage.getItem("task")); 
+    const newItem = createListItem(text); 
+    console.log(newItem)
+    const itemListTodo = document.querySelector(".item-list-todo"); 
+    const itemListDoing = document.querySelector(".item-list-doing")
+    
+}
+
+addTask.addEventListener("click", (ev) =>{
+   ev.preventDefault();
+   const task = document.getElementById("select-category").value;
+   const taskInput = document.querySelector(".task-title").value;
+   console.log(addItemToList(taskInput)); 
+
+   if (task === "todo") {
+        storage.todo.push(taskInput)
+        document.querySelector(".task-title").value = "";
+   } 
+   else if(task === "doing") {
+        storage.doing.push(taskInput)
+        document.querySelector(".task-title").value = "";
+   }
+   
+   else {
+        // storage.done.push(taskName.value)
+        // document.querySelector(".done").innerText = taskName.value; 
+        // document.querySelector(".task-title").value
+   }
+    localStorage.setItem("task", JSON.stringify(storage));
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     // console.log(getStoredObject);
+//     let todo = document.querySelector(".to-do");
+//     let doing = document.querySelector(".doing"); 
+//     if (getStoredObject.todo) {
+//         for(let key in getStoredObject.todo){
+//             console.log(getStoredObject.todo[key])
+//             const newkey = getStoredObject.todo[key]; //             
+//             // let checkbox = document.createElement("input").setAttribute('type', 'checkbox');
+//             // checkbox.classList.add("checkbox"); 
+//             // let listItem = document.createElement("li")
+//             // listItem.textContent = newkey; 
+//             // todo.appendChild(listItem);
+//         }
+//     }
+// if (getStoredObject.doing){
+        
+//         for(let x in getStoredObject.doing){
+//             const doingItem = getStoredObject.doing[x]; 
+//             console.log(doingItem)
+//             let doingList = document.createElement("li")
+//             doingList.textContent = doingItem; 
+//             doing.appendChild(doingList);
+//         }
+//     }
+
+
+    
+// })
+
+// window.addEventListener('load', function(ev) {
 //     ev.preventDefault();
-//     openModal.classList.remove('hidden'); 
-//     openModa.style.transition = '0.3s'
-// }
 
+//     const getStoredObject = JSON.parse(localStorage.getItem("task")); 
+//     // console.log(getStoredObject);
+//     let todo = document.querySelector(".to-do");
+//     let doing = document.querySelector(".doing"); 
+//     if (getStoredObject.todo) {
+//         for(let key in getStoredObject.todo){
+//             console.log(getStoredObject.todo[key])
+//             const newkey = getStoredObject.todo[key]
+//             let listItem = document.createElement("li")
+//             listItem.textContent = newkey; 
+//             todo.appendChild(listItem);
+//         }
+//     }
 
-// Close the modal box 
-// const closeModal = function(ev){
-//     ev.preventDefault();
-//     plusModal.classList.add('hidden'); 
-// }
-
-// plusIcon.addEventListener('click', openModal);
-// plusModalCancel.addEventListener('click', closeModal);
-
-// handBudger function
-// handBudger.addEventListener('click', () => {
-//   sideNav.classList.toggle('sideNav-active')
-// });
-
-// const openModal = document.querySelector('.'); 
-// const openModal = function(){
-//     // alert('Hello'); 
-//     plusModal.classList.remove('hidden'); 
-// }
-// plusModal.addEventListener('click', openModal ); 
-
-
-
-// const closeModal = function(){
-//     pl
-// }
-
-
-
-// const hiddenNav = document.querySelector('.side-nav'); 
-// // Navbar toggle 
-// const showNav = document.querySelector('.show').onclick = () =>{
-//     hiddenNav.classList.toggle('hidden');
-// }
-
-
-// // Accordion checker 
-// const acc = document.querySelector('.accordion-history').addEventListener('click', () => {
-//     const accDetail = document.querySelector('.accordion-detail').classList.toggle('accordion-detail-show'); 
-//     const arrowDown = document.querySelector('.arrow').classList.toggle('fa-arrow-alt-circle-down');
+//     if (getStoredObject.doing){
+//         for(let x in getStoredObject.doing){
+//             const doingItem = getStoredObject.doing[x]; 
+//             console.log(doingItem)
+//             let doingList = document.createElement("li")
+//             doingList.textContent = doingItem; 
+//             doing.appendChild(doingList);
+//         }
+//     }
 // }); 
-
-
-// const addPlus = document.querySelector('.add-task-plus').addEventListener('click', () => {
-//     alert('Hey'); 
-// }); 
-
-// // Add task modal box
-// const addTask = document.querySelectorAll('.add-task').forEach((item) => {
-//     item.addEventListener('click', () => {
-//         alert('alert'); 
-//     })
-// }); 
-
-
